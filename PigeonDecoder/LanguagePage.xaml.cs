a7995f36b98978e4f897f74b7718218d251156ab
Satvik Rajnarayanan <72124172+Satvik-web@users.noreply.github.com>
2022-03-08 06:34:03 +0530

    Add files via upload

---
+++ PigeonDecoder/LanguagePage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PigeonDecoder
+{
+    public partial class LanguagePage : ContentPage
+    {
+        public LanguagePage()
+        {
+            InitializeComponent();
+
+            langlabel.Text = Util.Language.Contains("T") ? "Click on the above button..." : "மேலே உள்ள பொத்தானை அழுத்தவும்...";
+
+            tamillabel.IsVisible = adlabel.IsVisible = adlabel.IsEnabled = Util.Language.Contains("T");
+
+            LangImage.Source = ImageSource.FromFile(Util.Language.Contains("T") ? "tamil.png" : "english.png");
+        }
+
+        async void ChangeLang_Tapped(System.Object sender, System.EventArgs e)
+        {
+            if (Util.Language.Contains("E") || Util.Language.Contains("e"))
+            {
+                Util.Language = "T";
+                Util.ChangeLanguage("T");
+                adlabel.IsEnabled = false;
+                adlabel.IsVisible = false;
+                tamillabel.IsVisible = true;
+                LangImage.Source = ImageSource.FromFile("tamil.png");
+                langlabel.Text = "Click on the above button...";
+                await DisplayAlert("மொழி மாற்றப்பட்டுள்ளது", "மொழி தமிழாக மாறிவிட்டது", "சரி");
+                await DisplayAlert("தமிழ் தாய்யைக் காபாற்றவும்!!!", "நம் தாய்மொழி ஆகிய தமிழ்மொழியை, நாம் அனைவரும் போற்றி புகழ்ந்து காப்பாற்றவேண்டும்!", "தமிழ் மொழி வாழ்க! தமிழ் மொழி வாழ்க! என்றேன்றும் தமிழ் மொழி வாழ்க!");
+            }
+            else
+            {
+                Util.Language = "E";
+                Util.ChangeLanguage("E");
+                adlabel.IsEnabled = true;
+                adlabel.IsVisible = true;
+                tamillabel.IsVisible = false;
+                langlabel.Text = "மேலே உள்ள பொத்தானை அழுத்தவும்...";
+                LangImage.Source = ImageSource.FromFile("english.png");
+                await DisplayAlert("The Language has been changed", "The Language has been changed to English", "Ok");
+            }
+            MainPage.LangCommand.Execute(null);
+            Create.LangCommand.Execute(null);
+            AdsPage.LangCommand.Execute(null);
+        }
+    }
+}
---
+++ PigeonDecoder/Util.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace PigeonDecoder
+{
+    public class Util
+    {
+        public static string Language { get; set; }
+        public static void ChangeLanguage(string p)
+        {
+            File.WriteAllText(Path.Combine(FileSystem.AppDataDirectory, "Lang.txt"), p);
+        }
+
+        public static string[] DeserialiseEmail(string param)
+        {
+            string raw = param.Remove(0, 10);
+            string[] splitter = Split(';', raw);
+            string mailid = splitter[0];
+            string nextraw = splitter[1];
+            string[] splitter2 = Split(';', nextraw);
+            string subject = splitter2[0].Remove(0, 4);
+            string body = splitter2[1].Remove(0, 5);
+            body = Split(';', body)[0];
+            string[] output = new string[]
+            {
+                mailid,
+                subject,
+                body
+            };
+            return output;
+        }
+
+        public static string[] DeserialiseSms(string param)
+        {
+            string raw = param.Remove(0, 6);
+            string[] output = Split(':', raw);
+            return output;
+        }
+
+        public static string[] DeserialiseWIFI(string param)
+        {
+            param = param.Remove(0, 7);
+            string[] raw = Split(';', param);
+            string SSID = raw[0].Replace(";", "");
+            string Password = Split(';', raw[1])[1].Replace("P:", "").Replace(";", "");
+            return new string[] { SSID, Password };
+        }
+
+        private static string[] Split(char key, string param)
+        {
+            char[] spearator = { key, ' ' };
+            int count = 2;
+            string[] strlist = param.Split(spearator, count, StringSplitOptions.None);
+            return strlist;
+        }
+
+        public static string SerialiseEmail(string maidid, string sub, string msg)
+        {
+            string startprefix = "MATMSG:TO:";
+            string subprefix = "SUB:";
+            string bodyprefix = "BODY:";
+            string endprefix = ";";
+            string Code = startprefix + maidid + endprefix + subprefix + sub + endprefix + bodyprefix + endprefix + endprefix;
+            return Code;
+        }
+
+        public static string SerialiseSMS(string phno, string msg)
+        {
+            string startprefix = "SMSTO:";
+            string splitprefix = ":";
+            string Code = startprefix + phno + splitprefix + msg;
+            return Code;
+        }
+
+        public static string SerialiseWifi(string SSID, string password)
+        {
+            string startprefix = "WIFI:S:";
+            string typeprefix = "T:WPA";
+            string passwordprefix = "P:";
+            string endprefix = ";";
+            string Code = startprefix + SSID + endprefix + typeprefix + endprefix + passwordprefix + password + endprefix + endprefix;
+            return Code;
+        }
+
+        public static void CreateCode(string param, string CodeType)
+        {
+            MainPage.NavigateCommand.Execute(new CodePage(param, CodeType));
+        }
+
+        public static bool ContainsLetter(string s)
+        {
+            if (String.IsNullOrEmpty(s))
+                return false;
+
+            return Regex.IsMatch(s, @"^[a-zA-Z]+$");
+        }
+    }
+}
